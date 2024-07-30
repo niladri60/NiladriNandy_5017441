6c6ccb587939340470bcdf18cc97945a2f48a4ed
niladri <niladri.stud@gmail.com>
2024-07-30 22:25:11 +0530

    Add Exercise 5

---
+++ Week 1 Assesment/Week 1_Algorthms_Data Structures/Task_Management_System/src/taskDocs/TaskLinkedList.java
@@ -0,0 +1,60 @@
+package taskDocs;
+
+public class TaskLinkedList {
+    private Node head;
+
+    // Add a task to the linked list
+    public void addTask(Task task) {
+        Node newNode = new Node(task);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Search for a task by ID
+    public Task searchTaskById(String taskId) {
+        Node current = head;
+        while (current != null) {
+            if (current.task.getTaskId().equals(taskId)) {
+                return current.task;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    // Traverse and display all tasks
+    public void traverseTasks() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.task);
+            current = current.next;
+        }
+    }
+
+    // Delete a task by ID
+    public boolean deleteTaskById(String taskId) {
+        Node current = head;
+        Node previous = null;
+
+        while (current != null) {
+            if (current.task.getTaskId().equals(taskId)) {
+                if (previous == null) {
+                    head = current.next; // Deleting the head node
+                } else {
+                    previous.next = current.next;
+                }
+                return true;
+            }
+            previous = current;
+            current = current.next;
+        }
+        return false;
+    }
+}
