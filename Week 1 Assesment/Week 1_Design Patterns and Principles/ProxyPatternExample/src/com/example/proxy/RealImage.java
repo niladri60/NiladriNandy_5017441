3302580c164e2c3f181a7a8e2565f10ba39bb217
niladri <niladri.stud@gmail.com>
2024-07-30 13:04:14 +0530

    Add Question 6

---
+++ Week 1 Assesment/Week 1_Design Patterns and Principles/ProxyPatternExample/src/com/example/proxy/RealImage.java
@@ -0,0 +1,25 @@
+package com.example.proxy;
+
+public class RealImage implements Image {
+    private String filename;
+
+    public RealImage(String filename) {
+        this.filename = filename;
+        loadImageFromDisk();
+    }
+
+    private void loadImageFromDisk() {
+        System.out.println("Loading " + filename + " from disk...");
+        // Simulate a delay for loading an image
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + filename);
+    }
+}
---
+++ Week 1 Assesment/Week 1_Design Patterns and Principles/ProxyPatternExample/src/com/example/proxy/TestProxyPattern.java
@@ -0,0 +1,20 @@
+package com.example.proxy;
+
+public class TestProxyPattern {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("photo1.jpg");
+        Image image2 = new ProxyImage("photo2.jpg");
+
+        // Image will be loaded from disk and displayed
+        image1.display();
+
+        // Image will be displayed directly from cache
+        image1.display();
+
+        // Image will be loaded from disk and displayed
+        image2.display();
+
+        // Image will be displayed directly from cache
+        image2.display();
+    }
+}
