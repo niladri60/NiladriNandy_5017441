8aff4a115c179d2d2c523c26d994c524fe49f11b
niladri <niladri.stud@gmail.com>
2024-07-30 13:00:16 +0530

    Add Question 5

---
+++ Week 1 Assesment/Week 1_Design Patterns and Principles/DecoratorPatternExample/src/com/example/decorator/TestDecoratorPattern.java
@@ -0,0 +1,14 @@
+package com.example.decorator;
+
+public class TestDecoratorPattern {
+    public static void main(String[] args) {
+        Notifier emailNotifier = new EmailNotifier();
+        emailNotifier.send("This is an email notification.");
+
+        Notifier emailAndSmsNotifier = new SMSNotifierDecorator(new EmailNotifier());
+        emailAndSmsNotifier.send("This is an email and SMS notification.");
+
+        Notifier emailSmsSlackNotifier = new SlackNotifierDecorator(new SMSNotifierDecorator(new EmailNotifier()));
+        emailSmsSlackNotifier.send("This is an email, SMS, and Slack notification.");
+    }
+}
