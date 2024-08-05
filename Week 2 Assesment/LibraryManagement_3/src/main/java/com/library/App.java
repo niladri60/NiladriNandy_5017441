c0ac43c6dfb9460150a0d7e7b918b19db36745b4
niladri60 <niladri.nandy2names@gmail.com>
2024-08-05 23:23:14 +0530

    Add Exercise 3

--- Week 2 Assesment/LibraryManagement_3/src/main/java/com/library/App.java
+++ Week 2 Assesment/LibraryManagement_3/src/main/java/com/library/App.java
@@ -1,13 +1,15 @@
 package com.library;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = (BookService) context.getBean("bookService");
+        bookService.manageBooks();
     }
 }
