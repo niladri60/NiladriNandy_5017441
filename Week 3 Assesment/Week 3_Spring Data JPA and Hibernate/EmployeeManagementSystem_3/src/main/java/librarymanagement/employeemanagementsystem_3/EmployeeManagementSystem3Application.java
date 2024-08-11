6d14530b3b2fd95ed399b4e322e8986cd1e0d8d5
niladri60 <niladri.nandy2names@gmail.com>
2024-08-12 01:50:02 +0530

    Add Exercise 3

---
+++ Week 3 Assesment/Week 3_Spring Data JPA and Hibernate/EmployeeManagementSystem_3/src/main/java/librarymanagement/employeemanagementsystem_3/EmployeeManagementSystem3Application.java
@@ -0,0 +1,13 @@
+package librarymanagement.employeemanagementsystem_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystem3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementSystem3Application.class, args);
+    }
+
+}
