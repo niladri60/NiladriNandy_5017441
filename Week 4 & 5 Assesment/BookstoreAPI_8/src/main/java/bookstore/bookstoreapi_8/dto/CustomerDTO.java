cbbf17bbf6d54e0e94ba6a36d64ae7d0b64beb4a
niladri60 <niladri.nandy2names@gmail.com>
2024-08-21 01:16:55 +0530

    Remove Exercise 8

--- Week 4 & 5 Assesment/BookstoreAPI_8/src/main/java/bookstore/bookstoreapi_8/dto/CustomerDTO.java
+++
@@ -1,7 +0,0 @@
-package bookstore.bookstoreapi_8.dto;
-
-public class CustomerDTO {
-    private Long id;
-    private String name;
-    private String email;
-}
--- Week 4 & 5 Assesment/BookstoreAPI_8/src/main/java/bookstore/bookstoreapi_8/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package bookstore.bookstoreapi_8.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
